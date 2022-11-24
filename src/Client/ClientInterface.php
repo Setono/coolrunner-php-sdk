fca7cd72ddfe4cabfde321d14ac1b9e0a2e7455c
Joachim Løvgaard <joachim@loevgaard.dk>
2022-11-24 22:14:17 +0100

    Add missing method

--- src/Client/ClientInterface.php
+++ src/Client/ClientInterface.php
@@ -5,11 +5,14 @@ declare(strict_types=1);
 namespace Setono\CoolRunner\Client;
 
 use Psr\Http\Message\ResponseInterface;
+use Setono\CoolRunner\Client\Endpoint\ProductsEndpointInterface;
 use Setono\CoolRunner\Client\Endpoint\ServicepointsEndpointInterface;
 
 interface ClientInterface
 {
     public function get(string $uri): ResponseInterface;
+
+    public function products(): ProductsEndpointInterface;
 
     public function servicepoints(): ServicepointsEndpointInterface;
 }
